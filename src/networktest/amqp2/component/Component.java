e607a601faffdd3f02e9e8be08dca7c9f4564322
Alexander <alexandersvae@gmail.com>
2015-10-14 17:22:35 +0200

    Remove used files

--- src/networktest/amqp2/component/Component.java
+++
@@ -1,7 +0,0 @@
-package networktest.amqp2.component;
-
-import no.ntnu.item.arctis.runtime.Block;
-
-public class Component extends Block {
-
-}
--- src/networktest/communication/Communication.java
+++
@@ -1,7 +0,0 @@
-package networktest.communication;
-
-import no.ntnu.item.arctis.runtime.Block;
-
-public class Communication extends Block {
-
-}
